274c131778c7d1496555311dd4a968a8328ab1a0
Steve Ferreira Da Silva <steve.silva@gpssa.com.br>
2025-02-19 08:04:23 -0300

    Atualização do endereço do curso de matemática

--- src/app/curso-alfabetizacao-matematica/Address/index.tsx
+++ src/app/curso-alfabetizacao-matematica/Address/index.tsx
@@ -6,10 +6,10 @@ const Address: React.FC = () => {
             <h1 className="text-3xl font-bold text-white mb-4">Endereço</h1>
 
             <h3 className="list-disc list-inside text-lg text-white">
-                Colégio crescer
+                Espaço de Recreação Crescer
             </h3>
             <span className='text-lg text-white'>
-                Av. Rua Maria Helena, 68, Centro, Carapicuíba - SP. Cep: 06320-070
+                Av. Miriam, 28 - Centro, Carapicuíba - SP, 06320-060
             </span>
         </section>
     );
