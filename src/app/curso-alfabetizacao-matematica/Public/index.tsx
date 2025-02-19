a77ba5945ff0c0249318193b54f906f299c72a56
Steve Ferreira Da Silva <steve.silva@gpssa.com.br>
2025-02-19 08:08:12 -0300

    alteração nos horários e turmas do curso de matemática.

--- src/app/curso-alfabetizacao-matematica/Public/index.tsx
+++ src/app/curso-alfabetizacao-matematica/Public/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import { Container } from './styles';
-
 const Public: React.FC = () => {
     return (
         <section className='flex flex-col gap-4 p-4 py-10'>
@@ -11,77 +9,33 @@ const Public: React.FC = () => {
                     Atendemos a um público de idades variadas
                     desde os primeiros anos do ensino fundamental ao final deste ciclo.
                 </p>
+                <p className="text-md text-gray-600 text-center font-semibold">
+                    * Os horários abaixo são válidos para todas as quartas-feiras.
+                </p>
             </div>
             <div className='flex flex-col md:flex-row gap-4 justify-center'>
-                <div className='shadow-lg w-full md:w-60 h-80'>
-                    <figure className='h-40 w-full bg-gray-700'>
-                        {/* <Image
-                            src="https://d9aloqs890lqz.cloudfront.net/uploads/846/17-07-2024/5y4uo-5.jpg"
-                            alt="materiais_pedagogicos"
-                            width={100}
-                            height={100}
-                            priority
-                        /> */}
+                <div className='shadow-lg w-full md:w-96 h-80'>
+                    <figure className='h-40 w-full bg-blue-100'>
                     </figure>
                     <div className='flex flex-col justify-center items-center mt-4'>
-                        <strong className='text-xl'>Fundamental 1</strong>
-                        <p>Crianças de 6 à 10 Anos</p>
-                        <span>Horário: 10h30 Às 12h</span>
+                        <strong className='text-xl text-blue_d'>Ensino Fundamental I</strong>
+                        <p>Crianças de 6 a 10 Anos (2º ao 5º ano)</p>
+                        <span>08:00 às 09:30 (manhã)</span>
+                        <span>15:30 às 17:00 (tarde)</span>
                     </div>
                 </div>
-                <div className='shadow-lg  w-full md:w-60 h-80'>
-                    <figure className='h-40 w-full bg-gray-700'>
-                        {/* <Image
-                            src="https://d9aloqs890lqz.cloudfront.net/uploads/846/17-07-2024/5y4uo-5.jpg"
-                            alt="materiais_pedagogicos"
-                            width={100}
-                            height={100}
-                            priority
-                        /> */}
+                <div className='shadow-lg  w-full md:w-96 h-80'>
+                    <figure className='h-40 w-full bg-blue-100'>
                     </figure>
                     <div className='flex flex-col justify-center items-center mt-4'>
-                        <strong className='text-xl'>Ensino Fundamental 2</strong>
-                        <p>Crianças de 11 à 13 Anos</p>
-                        <span>Horário: 8h às 10h</span>
-                    </div>
-                </div>
-
-                <div className='shadow-lg  w-full md:w-60 h-80'>
-                    <figure className='h-40 w-full bg-gray-700'>
-                        {/* <Image
-                            src="https://d9aloqs890lqz.cloudfront.net/uploads/846/17-07-2024/5y4uo-5.jpg"
-                            alt="materiais_pedagogicos"
-                            width={100}
-                            height={100}
-                            priority
-                        /> */}
-                    </figure>
-                    <div className='flex flex-col justify-center items-center mt-4'>
-                        <strong className='text-xl'>Ensino Médio</strong>
-                        <p>Jovens de 14 à 17 Anos</p>
-                        <span>Horário: 14h às 16h</span>
-                    </div>
-                </div>
-
-                <div className='shadow-lg  w-full md:w-60 h-80'>
-                    <figure className='h-40 w-full bg-gray-700'>
-                        {/* <Image
-                            src="https://d9aloqs890lqz.cloudfront.net/uploads/846/17-07-2024/5y4uo-5.jpg"
-                            alt="materiais_pedagogicos"
-                            width={100}
-                            height={100}
-                            priority
-                        /> */}
-                    </figure>
-                    <div className='flex flex-col justify-center items-center mt-4'>
-                        <strong className='text-xl'>Pré vestibular</strong>
-                        <p>Jovens a partir de 16 anos</p>
-                        <span>Horário: 16h às 16h</span>
+                        <strong className='text-xl text-blue_d'>Ensino Fundamental II</strong>
+                        <p>Crianças de 11 à 13 Anos (6º ao 9º ano)</p>
+                        <span>09:45 às 11:00 (manhã)</span>
+                        <span>14:00 às 15:15 (tarde)</span>
                     </div>
                 </div>
             </div>
         </section>
-
     );
 }
 
